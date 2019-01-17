d8abbedddf3f267f68e19023947f3de967955bbc
Jack <ajackofsometrades@gmail.com>
2019-01-16 18:59:27 -0800

    working on chart (showing work)

--- client/src/views/content/orders/graph/Chart.js
+++ client/src/views/content/orders/graph/Chart.js
@@ -6,8 +6,35 @@ import { Bar as BarChart } from 'react-chartjs-2';
 
 const OrderChart = ({ orders }) => {
 
+
+  const BARS = 10;
+
+  orders.sort((a, b) => a.price - b.price);
+
+  // const range = {
+  //   high: orders[0].price,
+  //   low: orders[orders.length - 1].price,
+  // };
+
+  const range = orders.reduce((acc, curr) => {
+    if (typeof acc.high !== 'number' || acc.high < curr.price) acc.high = curr.price;
+    if (typeof acc.low !== 'number' || acc.low > curr.price) acc.low = curr.price;
+    return acc;
+  }, { high: null, low: null });
+
+  const interval = (range.high - range.low) / BARS;
+
+  const labels = Array.apply(null, { BARS })
+    .map((label, index) => {
+      return (interval * index) + range.low;
+    });
+
+  const data = orders.reduce((acc, curr) => {
+
+  }, [])
+
   const chartData = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    labels,
     datasets: [
       {
         fillColor: '#25BDFF',
@@ -16,22 +43,18 @@ const OrderChart = ({ orders }) => {
         pointStrokeColor: '#fff',
         pointHighlightFill: '#fff',
         pointHighlightStroke: '#25BDFF',
-        data: [28, 48, 40, 19, 86, 27, 90]
+        data: [28, 48, 40, 19, 86, 27, 90, 23, 23, 32]
       }
     ]
   };
 
   const chartOptions = {
+    redraw: true,
     bezierCurve: false,
     datasetFill: false,
     pointDotStrokeWidth: 4,
     scaleShowVerticalLines: false,
-    responsive: true
-  };
-
-  const chartSize = {
-    height: 300,
-    width: 300
+    // responsive: true
   };
 
   return (
@@ -40,8 +63,8 @@ const OrderChart = ({ orders }) => {
       <BarChart
         data={chartData}
         options={chartOptions}
-        width={chartSize.width}
-        height={chartSize.height}
+        width={300}
+        height={300}
       />
     </div>
   );
