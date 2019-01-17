66efa8fda2db8f832337fc2192c7f345aab5820e
Jack <ajackofsometrades@gmail.com>
2019-01-16 17:01:11 -0800

    initial post route is working

---
+++ server/src/resources/orders/routes.js
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+import Order from './model';
+
+export default Router()
+
+  .get('/items/:id', (req, res, next) => {
+    const { id } = req.params;
+    Order.findById(id)
+      .lean()
+      .then(order => res.json(order))
+      .catch(next);
+  })
+
+  .get('/orders', (req, res, next) => {
+    Order.find()
+      .lean()
+      .then(orders => res.json(orders))
+      .catch(next);
+  })
+
+  .post('/orders', (req, res, next) => {
+    const { ticker, side, price, shares, open } = req.body;
+    Order.create({ ticker, side, price, shares, open })
+      .then(order => res.json(order))
+      .catch(next);
+  });
