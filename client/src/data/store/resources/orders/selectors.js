dc84c6d40d0f8cccb18e512881874016a6876211
Jack <ajackofsometrades@gmail.com>
2019-01-16 17:41:57 -0800

    adding selector for ticker orders

--- client/src/data/store/resources/orders/selectors.js
+++ client/src/data/store/resources/orders/selectors.js
@@ -1,3 +1,6 @@
 export const getOrders = state => state.orders.list;
 
 export const getOrder = state => state.orders.details;
+
+export const getTickerOrders = state => state.orders.list
+  .filter(order => order.ticker === state.selectedTicker);
