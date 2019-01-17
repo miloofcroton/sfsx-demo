47da8fac348b05fce08040d1a36e872bf3afa2b8
Jack <ajackofsometrades@gmail.com>
2019-01-16 18:26:07 -0800

    now correctly passing orders down to chart component

--- client/src/data/store/resources/orders/selectors.js
+++ client/src/data/store/resources/orders/selectors.js
@@ -3,4 +3,4 @@ export const getOrders = state => state.orders.list;
 export const getOrder = state => state.orders.details;
 
 export const getTickerOrders = state => state.orders.list
-  .filter(order => order.ticker === state.selectedTicker);
+  .filter(order => order.ticker === state.orders.selectedTicker);
